e1d9fbfe9b631de6192710f997686f3d7c2168d4
Anne Sophie <annesophie@Annes-MacBook-Pro.local>
2018-08-30 07:40:16 -0700

    Add new routes and footer

--- app.py
+++ app.py
@@ -7,14 +7,36 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/add")
-def add_device():
-    return "Add a new device"
-
-@app.route("/login")
-def login():
-    return "login"
+# Show device details
+@app.route("/device/")
+def show_device():
+    return "Show Device details"
 
 @app.route("/user/username/")
 def profile():
-    return "Profile page"
+   return "Profile page"
+
+# Add new device
+@app.route("/device/new/")
+def add_device():
+    return "Add new Device"
+
+# Edit device
+@app.route("/edit/1")
+def edit_device():
+    return "Edit device"
+
+# Delete device
+app.route("/device/1/delete")
+def delete_device():
+    return "Delete device"
+
+# Login
+app.route("/login")
+def login():
+    return "Login"
+
+# Sign up
+app.route("/register")
+def register():
+    return "Register"
